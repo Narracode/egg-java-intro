0a234de7e9f54e0da36a300678e615ec3f33430f
Narracode <zeronarratore@gmail.com>
2023-05-30 17:16:14 -0600

    Ejercicio 9 Escriba un programa que pida una frase o palabra y valide si la primera letra de esa frase es una ‘A’. Si la primera letra es una ‘A’, se deberá de imprimir un mensaje por pantalla que diga “CORRECTO”, en caso contrario, se deberá imprimir “INCORRECTO”. Nota: investigar la función Substring y equals() de Java.

---
+++ src/main/java/com/egg/ejerciciosjava/empiezaA.java
@@ -0,0 +1,25 @@
+package com.egg.ejerciciosjava;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author zero
+ */
+public class empiezaA {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        
+        System.out.println("Favor de ingresar una frase");
+        String frase = leer.nextLine();
+        String inicio = frase.substring(0,1);
+        confirma(inicio.equals("A"));
+    }
+    public static void confirma(boolean c) {
+        if (c) {
+            System.out.println("CORRECTO");
+        } else {
+            System.out.println("INCORRECTO");
+        }
+    }
+}
