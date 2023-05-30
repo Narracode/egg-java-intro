d610eb5cf5b2ba96adb1bc2c96115af3f2119d31
Narracode <zeronarratore@gmail.com>
2023-05-30 11:00:42 -0600

    Ejercicio 7 Crear un programa que pida una frase y si esa frase es igual a “eureka” el programa pondrá un mensaje de Correcto, sino mostrará un mensaje de Incorrecto. funcion equals().

---
+++ src/main/java/com/egg/ejerciciosjava/eureka.java
@@ -0,0 +1,23 @@
+package com.egg.ejerciciosjava;
+import java.util.Scanner;
+
+/**
+ *
+ * @author zero
+ */
+public class eureka {
+
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        
+        System.out.print("Ingresa una frase > ");
+        String frase = leer.nextLine();
+        
+        if (frase.equals("eureka")) {
+            System.out.println("Correcto");
+        } else {
+            System.out.println("Incorrecto");
+        }
+    }
+    
+}
