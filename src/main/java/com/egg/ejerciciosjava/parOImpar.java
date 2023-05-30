4d50cb914336d2ca15d4e33541c2c531ee70af7a
Narracode <zeronarratore@gmail.com>
2023-05-30 17:31:45 -0600

    Procedimiento confirma Utilizar los mensajes correctos para el ejercicio

--- src/main/java/com/egg/ejerciciosjava/parOImpar.java
+++ src/main/java/com/egg/ejerciciosjava/parOImpar.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package com.egg.ejerciciosjava;
 import java.util.Scanner;
 
@@ -17,13 +13,15 @@ public class parOImpar {
         int num = leer.nextInt();
         confirma(esPar(num));
     }
+    
     public static void confirma(boolean c) {
         if (c) {
-            System.out.println("CORRECTO");
+            System.out.println("El numero es par");
         } else {
-            System.out.println("INCORRECTO");
+            System.out.println("El numero es impar");
         }
     }
+    
     public static boolean esPar(int n) {
         boolean par = n%2==0;
         return par;
