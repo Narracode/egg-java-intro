ded15568f763427303dfc649dd89ebd42e604a01
Narracode <zeronarratore@gmail.com>
2023-05-31 17:43:52 -0600

    Ejercicio 11 Realizar un programa que pida dos números enteros positivos por teclado y muestre por pantalla el siguiente menú: El usuario deberá elegir una opción y el programa deberá mostrar el resultado por pantalla y luego volver al menú. El programa deberá ejecutarse hasta que se elija la opción 5. Tener en cuenta que, si el usuario selecciona la opción 5, en vez de salir del programa directamente, se debe mostrar el siguiente mensaje de confirmación: ¿Está seguro que desea salir del programa (S/N)? Si el usuario selecciona el carácter ‘S’ se sale del programa, caso contrario se vuelve a mostrar el menú.

---
+++ src/main/java/com/egg/ejerciciosjava/menuOperaciones.java
@@ -0,0 +1,71 @@
+package com.egg.ejerciciosjava;
+import java.util.Scanner;
+
+/**
+ *
+ * @author zero
+ */
+public class menuOperaciones {
+    
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        String menu = " MENU\n1. Sumar\n2. Restar\n3. Multiplicar\n4. Dividir\n5. Salir";
+                
+        System.out.print("Ingrese cantidad A > ");
+        int numA = leer.nextInt();
+        System.out.print("Ingrese cantidad B > ");
+        int numB = leer.nextInt();
+        int opcion;
+        
+        do {
+            System.out.println(menu);
+            System.out.print("Elija opcion: >");
+            opcion = leer.nextInt();
+            switch(opcion) {
+                case 1:
+                    sumar(numA, numB);
+                break;
+                case 2:
+                    restar(numA, numB);
+                break;
+                case 3:
+                    multiplicar(numA, numB); 
+                case 4:
+                    dividir(numA, numB);
+                break;
+                case 5:
+                    System.out.println("Desea Salir? S/N");
+                    String meVoy = leer.next();
+                    if (meVoy.equalsIgnoreCase("s")){
+                        System.out.println("Bye");
+                    } else {
+                        opcion = 0;
+                }
+                break;
+                default:
+                    System.out.println("Opcion Invalida");
+            }
+        } while (opcion != 5);
+    }
+    
+    public static void sumar(int a, int b) {
+        int suma = a + b;
+        System.out.println("Suma= \n" + suma);
+    }
+    
+    public static void restar(int a, int b) {
+        int suma = a - b;
+        System.out.println("Resta= \n" + suma);
+    }
+    
+    public static void multiplicar(int a, int b) {
+        int producto = a * b;
+        System.out.println("Producto= \n" + producto);
+    }
+    
+    public static void dividir(int a, int b) {
+        double cociente = a / b;
+        System.out.println("Cociente= \n" + cociente);
+    }
+}
+
