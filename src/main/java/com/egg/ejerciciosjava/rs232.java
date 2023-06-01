20d174cb30004a09df25e7c5e1c1e395be54c6b9
Narracode <zeronarratore@gmail.com>
2023-05-31 18:56:40 -0600

    Validaciones Se agrega el lenght para evitar errores, se corrige el mensaje de resumen

--- src/main/java/com/egg/ejerciciosjava/rs232.java
+++ src/main/java/com/egg/ejerciciosjava/rs232.java
@@ -13,16 +13,21 @@ public class rs232 {
         int mal=0;
         do {
             System.out.print("Esperando mensaje > ");
-            String msg = leer.next();
-            if (msg.substring(0, 1).equals("X") && msg.substring(4,5).equals("O")) {
+            String msg = leer.nextLine();
+            if (msg.length()==5) {
+                if (msg.substring(0, 1).equals("X") && msg.substring(4,5).equals("O")) {
                 bien ++;
-            } else {
+                } else {
+                    mal ++;
+                }
+            }
+            else {
                 mal ++;
             }
             if (msg.equals("&&&&&")) {
                 logged = false;
             }
         } while (logged);
-        System.out.println("Lecturas Correctas" + bien + "Incorrectas" + mal);
+        System.out.println("Lecturas Correctas: " + bien + "Incorrectas: " + mal);
     }
 }
