4225cbd280c754efb3846203c910dd87c1bb57e6
Narracode <zeronarratore@gmail.com>
2023-06-01 16:43:14 -0600

    Ejercicio 14 Crea una aplicación que a través de una función nos convierta una cantidad de euros introducida por teclado a otra moneda, estas pueden ser a dólares, yenes o libras. La función tendrá como parámetros, la cantidad de euros y la moneda a converir que será una cadena, este no devolverá ningún valor y mostrará un mensaje indicando el cambio (void). El cambio de divisas es:  * 0.86 libras es un 1 €     * 1.28611 $ es un 1 €     * 129.852 yenes es un 1 €

---
+++ src/main/java/com/egg/ejerciciosjava/convertirDivisas.java
@@ -0,0 +1,51 @@
+package com.egg.ejerciciosjava;
+import java.util.Scanner;
+
+/**
+ *
+ * @author zero
+ */
+public class convertirDivisas {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        boolean logged = true;
+        String menu = "\n*Divisas\nL- A libras\nU- A Dolares\nY- A Yenes\nS- Salir";
+        
+        System.out.print("¿Cuantos Euros traes? > ");
+        double eur = leer.nextDouble();
+
+        do {
+            System.out.println(eur + " Euros");
+            System.out.println(menu);
+            System.out.print("Ingrese Opcion > ");
+            String divisa = leer.next();
+            divisa = divisa.toLowerCase();
+            if (divisa.equals("s")) {
+                logged = false;
+            }
+            convertir(eur,divisa);
+        } while (logged);
+    } 
+    public static void convertir(double e, String d) {
+        switch(d) {
+            case "l":
+                double lib = e * 0.86;
+                System.out.println("Son " + lib + "Libras");
+                break;
+            case "u":
+                double usd = e * 1.28611;
+                System.out.println("Son " + usd + " Dolares");
+                break;
+            case "y":
+                double yen = e * 129.852;
+                System.out.println("Son " + yen + "Yenes");
+                break;
+            case "s":
+                System.out.println("Bye");
+                break;
+            default:
+                System.out.println("Opcion no valida");
+                break;
+        }
+    }
+}
