272fd0d1c4bf2ab08ee833cd625e9df2922b560d
Narracode <zeronarratore@gmail.com>
2023-06-02 17:25:06 -0600

    Ejercicio 18 Realizar un programa que rellene un matriz de 4 x 4 de valores aleatorios y muestre la traspuesta de la matriz. La matriz traspuesta de una matriz A se denota por B y se obtiene cambiando sus filas por columnas (o viceversa).

---
+++ src/main/java/com/egg/ejerciciosjava/matrizTraspuesta.java
@@ -0,0 +1,50 @@
+package com.egg.ejerciciosjava;
+
+/**
+ *
+ * @author zero
+ */
+public class matrizTraspuesta {
+    public static void main(String[] args) {
+        int n = 4;
+        int[][] matriz = new int[n][n];
+
+        
+        rellenarMatriz(matriz,n,n);
+        System.out.println("Matriz A:");
+        mostrarMatriz(matriz);
+        System.out.println("\nMatriz B");
+        transponMatriz(matriz,n,n);
+    }
+    
+    public static void mostrarMatriz(int[][] m) {
+        //System.out.println("Los valores dentro de la matriz: ");
+        for (int[] fila:m) {
+            String aux="";
+            for (int columna: fila) {
+                aux = aux + columna + " ";
+            }
+            System.out.println(aux);
+        }
+    }
+    
+    public static int[][] rellenarMatriz(int m[][], int kN, int kM) {
+        for (int i=0; i < kN; i++) {
+            for (int j = 0; j < kM; j++) {
+                m[i][j]= (int) ((Math.random()*10)*(Math.random()*10)
+                    *(Math.random()*10)*(Math.random()*10));
+            }
+        }
+        return m;
+    }
+    
+    public static void transponMatriz(int[][] m, int kN, int kM) {
+        for (int i=0; i < kN; i++) {
+            String aux = "";
+            for (int j = 0; j < kM; j++) {
+                aux= aux + m[j][i] + " ";
+            }
+            System.out.println(aux);
+        }
+    }    
+}
