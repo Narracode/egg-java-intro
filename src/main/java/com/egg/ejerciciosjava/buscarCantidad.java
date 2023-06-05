f58987b0b86fafe926dcbaa05393136a796775b9
Narracode <zeronarratore@gmail.com>
2023-06-05 17:33:48 -0600

    Refactorizar Se crean metodos para las tareas del problema.

--- src/main/java/com/egg/ejerciciosjava/buscarCantidad.java
+++ src/main/java/com/egg/ejerciciosjava/buscarCantidad.java
@@ -3,24 +3,47 @@ import java.util.Scanner;
 
 public class buscarCantidad {
     public static void main(String[] args) {
-        Scanner leer = new Scanner(System.in);
         int[] vector;
-        System.out.print("Ingrese la dimension del vector > ");
-        int n = leer.nextInt();
-        vector=new int[n];
-        int k = n-1;
-        int cuenta = 0;
         
-        for (int i=0;i < k;i++) {
-            vector[i]= (int)(Math.random()*10);
-        }
-        
-        System.out.print("Ingresa un numero a buscar > ");
-        int tgt = leer.nextInt();
-        
+        System.out.print("Dimension del vector");
+        vector = new int[leerCantidad(1,50)];
+        llenarVector(vector);
         mostrarVector(vector);
-        for (int i=0;i < k; i++) {
-            if(vector[i]==tgt) {
+        System.out.print("Numero a buscar");
+        buscarNumero(vector, leerCantidad(0,10));
+    }
+    
+    public static void mostrarVector(int[] v) {
+        String aux="";
+        for (int valor : v) {
+            aux = aux + valor + " ";
+        }
+        System.out.println(aux);
+    }
+    
+    public static int leerCantidad(int min, int max) {
+        Scanner leer = new Scanner(System.in);
+        System.out.print("Introduce un numero entre "+min+" y "+max+" > ");
+        int x = 0;
+        do {
+            x = leer.nextInt();
+            if (x<min && x>max) {
+                asiNo();
+            }
+        } while (x<min && x>max);
+        return x;
+    }
+    
+    public static void llenarVector(int[] v) {
+        for (int i=0;i < v.length ;i++) {
+            v[i]= (int)(Math.random()*10);
+        }
+    }
+    
+    public static void buscarNumero(int[] v, int tgt) {
+        int cuenta = 0;
+        for (int i=0;i < v.length; i++) {
+            if(v[i]==tgt) {
                 System.out.println("El numero " + tgt + " se encuentra en la posicion: " + i);
                 cuenta++;
             }
@@ -30,13 +53,5 @@ public class buscarCantidad {
         } else {
             System.out.println("No hay coincidencias");
         }
-    }
-    
-    public static void mostrarVector(int[] v) {
-        String aux="";
-        for (int valor : v) {
-            aux = aux + valor + " ";
-        }
-        System.out.println(aux);
     }
 }
